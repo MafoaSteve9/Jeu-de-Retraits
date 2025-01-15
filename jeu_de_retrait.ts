afc7d2b36e1e55da23fd5aef9edb566dd0b5e710
Steve Mafoa <steve_mafoa@icloud.com>
2025-01-15 17:13:23 +0100

    feat: Add constructor to initialize game state.

--- jeu_de_retrait.ts
+++ jeu_de_retrait.ts
@@ -4,5 +4,10 @@ class Jeu {
     private previousMove: number;
     private errorMessage: string;
   
-    
+    constructor() {
+        this.remaining = 10;
+        this.currentPlayer = 'Joueur 1';
+        this.previousMove = 0;
+        this.errorMessage = '';
+      }
 }
